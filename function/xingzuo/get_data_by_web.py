0d3cf0cd152883135f325838f2210ebf00346925
MXsong <2708506928@qq.com>
2023-05-11 12:04:24 +0800

    :sparkles: 优化获取`星座`数据功能，已有数据不会重复获取

--- function/xingzuo/get_data_by_web.py
+++ function/xingzuo/get_data_by_web.py
@@ -1,4 +1,6 @@
 import asyncio
+from datetime import datetime, timedelta
+
 import aiohttp
 from function.xingzuo import character
 from database.async_sqlite import AsyncSQLite
@@ -13,7 +15,7 @@ async_db = AsyncSQLite(db_name)
 
 async def init_xz_table() -> None:
     create_table_query = """
-    CREATE TABLE IF NOT EXISTS xinzuo (
+    CREATE TABLE IF NOT EXISTS xingzuo (
         time TEXT,
         xz_name TEXT,
         summary_star TEXT,
@@ -43,10 +45,30 @@ async def get_xz_data_by_web(time: Literal['day', 'tomorrow']) -> None:
     :param time: `day`表示获取今天的数据，`tomorrow`表示获取明天的数据
     :return: None
     """
+    async def existed_data(time: Literal['day', 'tomorrow']) -> list:
+        if time == 'day':
+            date = datetime.now().strftime('%Y%m%d')
+        else:
+            date = (datetime.now() + timedelta(days=1)).strftime('%Y%m%d')
+
+        query_sql = "SELECT xz_name FROM xingzuo WHERE time = ?"
+        xz_name_tuple_list = await async_db.fetch(query_sql, (date,))
+        xz_name_set = {xz_name for xz_name_tuple in xz_name_tuple_list for xz_name in xz_name_tuple}
+        logger.debug(f"{date}已存在{xz_name_set}星座的数据")
+
+        xz_name_all_set = set(character.xz_name_list)
+        need_xz_name_list = list(xz_name_all_set - xz_name_set)
+        logger.debug(f"{date}需要获取{need_xz_name_list}星座的数据")
+
+        return need_xz_name_list
+
     url = 'https://route.showapi.com/872-1'
     conf = Config()
     app_id = conf.SHOWAPI_ID
     app_secret = conf.SHOWAPI_SECRET
+
+    # 为节省API的使用次数，只查询没有的数据就行了
+    xz_name_list = await existed_data(time)
 
     # 创建一个最大并发数为5的信号量，我的API限制1秒一次，可以根据自己的API并发量调整
     sem = asyncio.Semaphore(1)
@@ -75,7 +97,7 @@ async def get_xz_data_by_web(time: Literal['day', 'tomorrow']) -> None:
                         xinzuo_data = tuple(xinzuo_obj.dict().values())
                         logger.debug(xinzuo_data)
 
-                        insert_query = 'INSERT OR REPLACE INTO XinZuo VALUES' \
+                        insert_query = 'INSERT OR REPLACE INTO xingzuo VALUES' \
                                        ' (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
                         await async_db.execute(insert_query, xinzuo_data)
                     else:
@@ -85,7 +107,7 @@ async def get_xz_data_by_web(time: Literal['day', 'tomorrow']) -> None:
             await asyncio.sleep(1)
 
     async with aiohttp.ClientSession() as session:
-        tasks = [fetch(session, url, xz_name) for xz_name in character.xz_name_list]
+        tasks = [fetch(session, url, xz_name) for xz_name in xz_name_list]
         await asyncio.gather(*tasks)
 
 # if __name__ == '__main__':
